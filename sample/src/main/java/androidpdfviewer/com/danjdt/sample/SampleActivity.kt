225521e05fb091bdb3fdf42bd177b16dacb6797b
Alexander <Sash0k.V@yandex.ru>
2021-10-05 08:30:12 +0300

    feat: open files from storage in sample app

--- sample/src/main/java/androidpdfviewer/com/danjdt/sample/SampleActivity.kt
+++ sample/src/main/java/androidpdfviewer/com/danjdt/sample/SampleActivity.kt
@@ -1,6 +1,10 @@
 package androidpdfviewer.com.danjdt.sample
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidpdfviewer.com.danjdt.sample.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import com.danjdt.pdfviewer.PdfViewer
@@ -9,9 +13,10 @@ import com.danjdt.pdfviewer.interfaces.OnPageChangedListener
 import com.danjdt.pdfviewer.utils.PdfPageQuality
 import com.danjdt.pdfviewer.view.PdfViewerRecyclerView
 import java.io.IOException
-import java.lang.Exception
 
-class SampleActivity : AppCompatActivity(), OnPageChangedListener , OnErrorListener{
+
+class SampleActivity : AppCompatActivity(), OnPageChangedListener , OnErrorListener {
+    private val REQUEST_CODE_LOAD = 367
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +34,40 @@ class SampleActivity : AppCompatActivity(), OnPageChangedListener , OnErrorListe
             .setOnPageChangedListener(this)
             .build()
             .load(R.raw.sample)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_open) {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "application/pdf"
+            }
+            startActivityForResult(intent,REQUEST_CODE_LOAD)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_LOAD && resultCode == Activity.RESULT_OK) {
+            data?.data?.let { uri ->
+                PdfViewer.Builder(binding.rootView)
+                    .view(PdfViewerRecyclerView(this))
+                    .setMaxZoom(3f)
+                    .setZoomEnabled(true)
+                    .quality(PdfPageQuality.QUALITY_1080)
+                    .setOnErrorListener(this)
+                    .setOnPageChangedListener(this)
+                    .build()
+                    .load(uri)
+            }
+        }
     }
 
     override fun onPageChanged(page: Int, total: Int) {
